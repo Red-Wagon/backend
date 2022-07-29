934b2a1e4d7159676144c26c5e1bbd0f7d15f375
sudoshreyansh <shreyanshjain14072002@gmail.com>
2022-07-30 01:48:52 +0530

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ## Red Wagon
 
+```shell
+yarn install    # Initial install
+yarn start      # Start server
+yarn lint       # Lint code
+yarn lint:fix   # Fix lint errors
+```
--- index.js
+++ index.js
@@ -15,22 +15,22 @@ const swaggerDocument = YAML.load('./openapi.yaml')
 app.set('etag', false)
 app.use(bodyParser.json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use(
-    OpenApiValidator.middleware({
-      apiSpec: './openapi.yaml',
-      validateRequests: true,
-      validateResponses: true,
-    }),
-);
+  OpenApiValidator.middleware({
+    apiSpec: './openapi.yaml',
+    validateRequests: true,
+    validateResponses: true
+  })
+)
 
 app.use('/', router)
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 500).json({
-        message: err.message,
-        errors: err.errors,
-    })
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors
+  })
 })
 
 app.listen(port, () => {
